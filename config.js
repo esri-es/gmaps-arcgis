7f47ca371ff897dacad34647d658e2b28841171f
Raul Jimenez Ortega <hhkaos@gmail.com>
2021-02-26 09:23:11 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const API_KEY_GMAPS = function(){
+    // Create one at: https://console.cloud.google.com/
+    return "AIzaSyAWrpf-_Xw1_kgzPBO_NoNdN2IWTFJLs-U";
+}
+const API_KEY_ARCGIS = function(){
+    // Create one at: https://developers.arcgis.com/api-keys
+    return "AAPK1806d768f4264875ab324828e5676bebrHnOSB3jCB3qjUNqNYih9ZJRSWj9Z9AX8P8ttYuZQfOCYkT_hzqSexu4EPhlpIWV";
+}
+
+export { API_KEY_GMAPS, API_KEY_ARCGIS };
